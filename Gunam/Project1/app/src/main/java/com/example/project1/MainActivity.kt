a167dbcae14f51701fcc5b6a136add76a9c40c3e
gunamkwon <rnska96@mju.ac.kr>
2021-11-01 19:57:42 +0900

    First Project - ViewPager2

---
+++ Gunam/Project1/app/src/main/java/com/example/project1/MainActivity.kt
@@ -0,0 +1,30 @@
+
+package com.example.project1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import com.example.project1.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val fragmentList = listOf(FragmentA(), FragmentB(), FragmentC(), FragmentD())
+        val adapter = FragmentAdapter(this)
+        adapter.fragmentList = fragmentList;
+
+        binding.viewPager.adapter = adapter
+
+        val tabTitles = listOf<String>("A", "B", "C", "D")
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) {tab,position->
+            tab.text = tabTitles[position]
+        }.attach()
+    }
+}
