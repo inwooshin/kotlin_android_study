e3d21adcfb0032681dd82498700101126e43effa
inwooshin <dkrlehowl45@naver.com>
2021-11-04 18:33:09 +0900

    버튼 기능 Fragment 쪽으로 옮김

--- inwoo/app/src/main/java/com/inwooshin/file_database/MainActivity.kt
+++ inwoo/app/src/main/java/com/inwooshin/file_database/MainActivity.kt
@@ -5,19 +5,24 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.inwooshin.file_database.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     //아래 코드로 할때는 버튼이 동작을 안했음 ㄷㄷㄷ
     //val binding by lazy {ActivityMainBinding.inflate(layoutflater)}
 
     private var mBinding : ActivityMainBinding? = null
     private val binding get() = mBinding!!
+    private var transaction : FragmentTransaction? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,56 +30,31 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
-            checkPermission()
-            Log.v("check", "버튼 클릭")
-        }
+        binding.bnvMain.setOnNavigationItemSelectedListener(this)
+
+        transaction = supportFragmentManager.beginTransaction()
+        transaction!!.add(R.id.fl_container, authority())
+        transaction!!.commit()
     }
 
-    private fun checkPermission(){
-        //카메라의 권한 승인상태 가져오기
-        val cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-
-        //저장된 값이 승인되었는지 아닌지 확인
-        if(cameraPermission == PackageManager.PERMISSION_GRANTED){
-            //승인 시 프로그램
-            startProcess()
-        }
-        else{
-            //미승인 이면 권한요청
-            requestPermission()
-        }
-    }
-
-    private fun startProcess() {
-        Toast.makeText(this, "카메라를 실행합니다.", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 99)
-
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        when(requestCode){
-            99 ->{
-                //권한 결괏값을 확인 후 실행 내용을 결정합니다.
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    startProcess()
-                }
-                else {
-                    finish()
-                }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        //TODO("Not yet implemented")
+        transaction = supportFragmentManager.beginTransaction()
+        when(item.itemId){
+            R.id.first -> {
+                transaction!!.replace(R.id.fl_container, authority())
+                Log.d("안녕", "안녕")
+            }
+            R.id.second -> {
+                transaction!!.replace(R.id.fl_container, fileAndData())
             }
         }
-    }
+        transaction!!.addToBackStack(null)
+        transaction!!.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        transaction!!.commit()
 
+        return true
+    }
 
 
 }
